2d512de8f8442fe60528538b55e2f79245d49b29
agussala2003 <asaladino@petersen.corp>
2023-12-07 00:10:46 -0300

    Hicimos la version opt

--- 20231206-23-Saladino-EventosKey/app.js
+++ 20231206-23-Saladino-EventosKey/app.js
@@ -1,22 +1,29 @@
+// const txtIng = document.getElementById('txtIng');
+// const txtMost = document.getElementById('txtMost');
+// const txtShow = document.getElementById('txtShow') ;
+// const arrConso = ['b','c','d','f','g','h','j','k','l','m','n','o','p','q','r','s','t','v','x','y','z'," "];
+// const arrVocal = ['a','e','i','o','u'];
+// let arrPal = []
+// txtIng.addEventListener("keyup", () => {
+//     arrPal = txtIng.value.split("")
+//     console.log(arrPal)
+// })
+// txtShow.addEventListener("click",()=> {
+//     arrPal.forEach(i => {
+//         if(arrVocal.includes(i)) {
+//             txtMost.innerText += '_';
+//         } else if (i == ' ') {
+//             txtMost.innerText += " "
+//         } else {
+//             txtMost.innerText += i
+//         }
+//     });
+// })
+
 const txtIng = document.getElementById('txtIng');
 const txtMost = document.getElementById('txtMost');
-const txtShow = document.getElementById('txtShow') ;
-const arrConso = ['b','c','d','f','g','h','j','k','l','m','n','o','p','q','r','s','t','v','x','y','z'," "];
-const arrVocal = ['a','e','i','o','u'];
-let arrPal = []
+let palabra = ""
 txtIng.addEventListener("keyup", () => {
-    arrPal = txtIng.value.split("")
-    console.log(arrPal)
-})
-txtShow.addEventListener("click",()=> {
-    arrPal.forEach(i => {
-        if(arrVocal.includes(i)) {
-            txtMost.innerHTML += '_';
-        } else if (i == ' ') {
-            console.log(true)
-            txtMost.innerHTML += "-"
-        } else {
-            txtMost.innerHTML += i
-        }
-    });
+    arrPal = txtIng.value.replace(/[aeiouAEIOU]/ig,"_");
+    txtMost.innerHTML = arrPal
 })
