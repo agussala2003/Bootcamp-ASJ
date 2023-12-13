5234639cd207ee101a3b8913a333b8564262c2fb
agussala2003 <asaladino@petersen.corp>
2023-12-13 11:26:47 -0300

    Algunos cambios en la app de Angular

--- my-angular-app-v16/src/app/components/main/card/card.component.ts
+++ my-angular-app-v16/src/app/components/main/card/card.component.ts
@@ -1,51 +1,91 @@
 import { Component } from '@angular/core';
 
+interface Product {
+  Name: string;
+  Url: string;
+  Category: string;
+  Desc: string;
+}
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
-  products = [
+  products: Product[] = [
     {
-      Name: 'Producto 1',
-      Url: 'https://dummyimage.com/600x400/000/fff',
-      Desc: 'Lorem ipsum dolor sit amet'
+      Name: 'Smartwatch',
+      Url: '../../../../assets/img/Smartwach.avif',
+      Category: 'Tech',
+      Desc: 'Lorem ipsum dolor sit amet',
     },
     {
-      Name: 'Producto 2',
-      Url: 'https://dummyimage.com/600x400/000/fff',
-      Desc: 'Lorem ipsum dolor sit amet'
+      Name: 'Licuadora',
+      Url: '../../../../assets/img/Licuadora.avif',
+      Category: 'Kitchen',
+      Desc: 'Lorem ipsum dolor sit amet',
     },
     {
-      Name: 'Producto 3',
-      Url: 'https://dummyimage.com/600x400/000/fff',
-      Desc: 'Lorem ipsum dolor sit amet'
+      Name: 'Monitor',
+      Url: '../../../../assets/img/Monitor.avif',
+      Category: 'Tech',
+      Desc: 'Lorem ipsum dolor sit amet',
     },
     {
-      Name: 'Producto 4',
-      Url: 'https://dummyimage.com/600x400/000/fff',
-      Desc: 'Lorem ipsum dolor sit amet'
+      Name: 'Heladera',
+      Url: '../../../../assets/img/Heladera.avif',
+      Category: 'Kitchen',
+      Desc: 'Lorem ipsum dolor sit amet',
     },
     {
-      Name: 'Producto 5',
-      Url: 'https://dummyimage.com/600x400/000/fff',
-      Desc: 'Lorem ipsum dolor sit amet'
+      Name: 'Teclado',
+      Url: '../../../../assets/img/Teclado.avif',
+      Category: 'Tech',
+      Desc: 'Lorem ipsum dolor sit amet',
     },
     {
-      Name: 'Producto 6',
-      Url: 'https://dummyimage.com/600x400/000/fff',
-      Desc: 'Lorem ipsum dolor sit amet'
+      Name: 'Sarten',
+      Url: '../../../../assets/img/Sarten.avif',
+      Category: 'Kitchen',
+      Desc: 'Lorem ipsum dolor sit amet',
     },
     {
-      Name: 'Producto 7',
-      Url: 'https://dummyimage.com/600x400/000/fff',
-      Desc: 'Lorem ipsum dolor sit amet'
+      Name: 'Horno',
+      Url: '../../../../assets/img/Horno.avif',
+      Category: 'Kitchen',
+      Desc: 'Lorem ipsum dolor sit amet',
     },
     {
-      Name: 'Producto 8',
-      Url: 'https://dummyimage.com/600x400/000/fff',
-      Desc: 'Lorem ipsum dolor sit amet'
-    }
-  ]
+      Name: 'Auriculares',
+      Url: '../../../../assets/img/Auris.avif',
+      Category: 'Tech',
+      Desc: 'Lorem ipsum dolor sit amet',
+    },
+  ];
+  addFlag = false;
+  techFlag = false;
+  kitchenFlag = false;
+  techProducts: Product[] = [];
+  kitchenProducts: Product[] = [];
+  filterTech() {
+    this.techFlag = true;
+    this.kitchenFlag = false;
+    this.techProducts = this.products.filter(
+      (item) => item.Category === 'Tech'
+    );
+  }
+  filterKitchen() {
+    this.techFlag = false;
+    this.kitchenFlag = true;
+    this.kitchenProducts = this.products.filter(
+      (item) => item.Category === 'Kitchen'
+    );
+  }
+  addProduct () {
+    this.addFlag = true;
+    setTimeout(() => {
+      this.addFlag = false;
+    },3000)
+  }
 }
